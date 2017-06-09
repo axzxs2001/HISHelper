40c3e064bc8ba1fa9fdecea0c2aab2ca96f719b4
桂素伟 <axzxs2001@163.com>
2017-06-09 08:56:36 +0800

    更新提交。

--- HISHelper/ProductReleaseSystem/Startup.cs
+++ HISHelper/ProductReleaseSystem/Startup.cs
@@ -24,14 +24,14 @@ namespace ProductReleaseSystem
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+          
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
