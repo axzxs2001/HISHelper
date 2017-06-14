48c183993fb3d9ae457b78af14b06b207fa959ef
xiaoliu7758521 <xiaoliu7758521@outlook.com>
2017-06-14 10:22:49 +0800

    下载提交

---
+++ HISHelper/ProductReleaseSystem/Controllers/DownLoadController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReleaseSystem.Controllers
+{
+    public class DownLoadController:Controller
+    {
+        //定义接口变量
+        IDownLoadFile _downLoadFile;
+
+        /// <summary>
+        /// 构函
+        /// </summary>
+        /// <param name="medicalRecords"></param>
+        public DownLoadController(IDownLoadFile downLoadFile)
+        {
+            _downLoadFile = downLoadFile;
+
+        }
+
+        /// <summary>
+        /// 页面入口
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        [HttpGet("download")]
+        public IActionResult DownLoad()
+        {
+            return View();
+        }
+    }
+}
---
+++ HISHelper/ProductReleaseSystem/Models/IRepository/IDownLoadFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReleaseSystem
+{
+    /// <summary>
+    /// 下载接口
+    /// </summary>
+    public  interface IDownLoadFile
+    {
+
+    }
+}
