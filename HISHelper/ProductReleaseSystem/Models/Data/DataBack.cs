f669352585915d7c32adfe0e79ebaa4b66413c6f
小凯 <2475653780@qq.com>
2017-06-15 10:39:09 +0800

    网页提交

---
+++ HISHelper/ProductReleaseSystem/Models/Data/DataBack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace noe1.Models
+{
+    public class DataBack
+    {
+        public int Result { get; set; }
+
+        public string Message { get; set; }
+
+        public List<Dictionary<string, dynamic>> Data { get; set; }
+    }
+}
