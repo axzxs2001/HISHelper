5c1e73cb4a54184ee0a09276380775c7efa9ef97
小凯 <2475653780@qq.com>
2017-06-29 09:45:27 +0800

    填加LowercaseContractResolver

---
+++ HISHelper/ProductReleaseSystem/Models/LowercaseContractResolver/Predicate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReleaseSystem
+{
+    /// <summary>
+    /// 访问路由谓词
+    /// </summary>s
+    public enum Predicate
+    {
+        None,
+        Get,
+        Post,
+        Put,
+        Delete
+    }
+}
