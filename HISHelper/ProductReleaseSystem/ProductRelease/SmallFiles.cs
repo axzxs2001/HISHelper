c710d4f332816bb88df69709fe22da138f9717cf
小凯 <2475653780@qq.com>
2017-07-11 08:43:38 +0800

    修改ip

---
+++ HISHelper/ProductReleaseSystem/ProductRelease/SmallFiles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReleaseSystem.ProductRelease
+{
+    public partial class SmallFiles
+    {
+        public int ID { get; set; }
+        public string SmallFileName { get; set; }
+        public DateTime UploadTime { get; set; }
+        public string SmallFilePath { get; set; }
+        public int SmallVersionsID { get; set; }
+
+
+        public virtual SmallVersions SmallVersions { get; set; }
+    }
+}
---
+++ HISHelper/ProductReleaseSystem/ProductRelease/SmallVersions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReleaseSystem.ProductRelease
+{
+    public partial class SmallVersions
+    {
+        public SmallVersions()
+        {
+            SmallFiles = new HashSet<SmallFiles>();
+        }
+
+        public int Id { get; set; }
+        public string SmallVersionNumber { get; set; }
+        public DateTime ReleaseTime { get; set; }
+        public string Publisher { get; set; }
+        public int VersionID { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<SmallFiles> SmallFiles { get; set; }
+        public virtual Versions version { get; set; }
+    }
+}
