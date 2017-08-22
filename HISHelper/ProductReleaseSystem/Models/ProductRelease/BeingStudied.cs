d2e43aa80925421fc086695f1b20c2d8ecc2f4de
小凯 <2475653780@qq.com>
2017-08-22 14:20:19 +0800

    项目提交

--- HISHelper/ProductReleaseSystem/Models/ProductRelease/BeingStudied.cs
+++ HISHelper/ProductReleaseSystem/Models/ProductRelease/BeingStudied.cs
@@ -8,7 +8,7 @@ namespace ProductReleaseSystem.ProductRelease
     public partial class BeingStudied
     {
         public int ID { get; set; }
-        public int DemandID { get; set; }
+        public int DemandID { get; set; } 
         public int DepartmentID { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime ExpectedTime { get; set; }
