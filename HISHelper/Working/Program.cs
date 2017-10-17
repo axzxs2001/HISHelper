daae1fb2a7ec4ebd29c0810b9468ca428c4d7db2
桂素伟 <axzxs2001@163.com>
2017-10-17 15:57:58 +0800

    添加当变色功能。

--- HISHelper/Working/Program.cs
+++ HISHelper/Working/Program.cs
@@ -19,7 +19,7 @@ namespace Working
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+                .UseStartup<Startup>().UseUrls("http://*:88")
                 .Build();
     }
 }
